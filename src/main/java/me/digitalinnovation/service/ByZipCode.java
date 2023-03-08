81cc0c08f4b7f963ddfe76e514cddc212793f53d
Renê Alves <renepvh11@gmail.com>
2023-03-08 16:30:31 -0400

    placing the annotation "FeignClient"

--- src/main/java/me/digitalinnovation/service/ByZipCode.java
+++ src/main/java/me/digitalinnovation/service/ByZipCode.java
@@ -1,9 +1,10 @@
 package me.digitalinnovation.service;
 
 import me.digitalinnovation.model.Address;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
+@FeignClient(name = "viacep", url = "https://viacep.com.br/ws")
 public interface ByZipCode {
     @GetMapping("/{zipCode}/json/")
     Address consultZipCode(@PathVariable("zipCode") String zipCode);
